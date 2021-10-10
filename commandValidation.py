d1a1dd2695cb2743b67942349a6b946372feea4b
kshitizsaini113 <kshitizsaini.rtk@gmail.com>
2021-10-10 13:32:51 +0530

    Command Validation

---
+++ commandValidation.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import time
+from pathlib import Path
+
+command = input("Enter the command: ")
+
+while command != 'sshExit':
+    commandFile = open("/.scripts/commandList", "r")
+    commandAvailable = commandFile.read()
+    commandAvailableList = commandAvailable.split("\n")
+
+    if command.split(' ', 1)[0] in commandAvailableList:
+        os.system(command)
+    else:
+        print("Comand permission not granted, please connect with the admin")
+
